baa4ac03428f9c4d7ea859cfc7c5bf22e1eb062c
Dubaman195 <dubaman195@gmail.com>
2025-10-07 15:21:41 +0100

    Feat: Complete Week 1 lab exercises

---
+++ week1_lab.py
@@ -0,0 +1,24 @@
+# Step 1
+"""
+print("Hello, World!")
+"""
+
+# Step 2
+# Storing text (a string)
+student_name = "Alex"
+
+
+# Storing a whole number (an integer)
+student_age = 20
+
+# Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+# Step 3
+# A simple calculation
+num1 = 15
+num2 = 10
+total = num1 + num2
+
+print("The sum is:", total)
